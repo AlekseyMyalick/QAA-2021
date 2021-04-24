673bd3f49afc48b397a601b4c4468c42d0ce4654
Alexey Myalik <myalick2017@yandex.ru>
2021-04-24 20:33:14 +0300

    Add class Truck and implement it

---
+++ HW-3/HW-3/Truck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HW_3
+{
+    public class Truck : Vehicle
+    {
+        /// <summary>
+        /// Carrying capacity, which is measured in tons.
+        /// </summary>
+        public double CarryingCapacity { get; set; }
+
+        public Truck(double engineVolume, string transmissionType, int maximumSpeed, double carryingCapacity)
+            : base(engineVolume, transmissionType, maximumSpeed)
+        {
+            CarryingCapacity = carryingCapacity;
+        }
+
+        public override void GetFullinfo()
+        {
+            base.GetFullinfo();
+            Console.WriteLine("Carrying capacity: " + CarryingCapacity);
+        }
+    }
+}
