99e30c625b531fc3eea7bb7585cb750906206ee1
Alexey Myalik <myalick2017@yandex.ru>
2021-04-24 20:01:24 +0300

    Add class Vehicle and implement it

---
+++ HW-3/HW-3/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HW_3
+{
+    public abstract class Vehicle
+    {
+        /// <summary>
+        /// Engine volume, which is measured in liters.
+        /// </summary>
+        public double EngineVolume { get; set; }
+
+        public string TransmissionType { get; set; }
+
+        /// <summary>
+        /// Maximum speed, which is measured in kilometers per hour.
+        /// </summary>
+        public int MaximumSpeed { get; set; }
+
+        public Vehicle (double engineVolume, string transmissionType, int maximumSpeed)
+        {
+            EngineVolume = engineVolume;
+            TransmissionType = transmissionType;
+            MaximumSpeed = maximumSpeed;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\nEngine volume: " + EngineVolume + "l" +
+                              "\nTransmission type: " + TransmissionType +
+                              "\nMaximum speed: " + MaximumSpeed + "km/h");
+        }
+    }
+}
