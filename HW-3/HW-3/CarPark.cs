e5443b5d900a7a2e5f9e69657a1ebc8c7f032a04
Alexey Myalik <myalick2017@yandex.ru>
2021-04-24 23:06:18 +0300

    Add the class Autopark and implement it

---
+++ HW-3/HW-3/CarPark.cs
@@ -0,0 +1,19 @@
+﻿namespace HW_3
+{
+    public class CarPark
+    {
+        private Vehicle[] vehicles;
+
+        public Vehicle this[int index]
+        {
+            get
+            {
+                return vehicles[index];
+            }
+            set
+            {
+                vehicles[index] = value;
+            }
+        }
+    }
+}
