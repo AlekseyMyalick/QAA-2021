14fdd79dbe335d0c965bbd8b2aad5305a91c0f69
Alexey Myalik <myalick2017@yandex.ru>
2021-04-24 20:21:48 +0300

    Add class Car and implement it

---
+++ HW-3/HW-3/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW_3
+{
+    public class Car : Vehicle
+    {
+        public string EngineType { get; set; }
+
+        public Car (double engineVolume, string transmissionType, int maximumSpeed, string engineType)
+            : base(engineVolume, transmissionType, maximumSpeed)
+        {
+            EngineType = engineType;
+        }
+
+        public override void GetFullinfo()
+        {
+            base.GetFullinfo();
+            Console.WriteLine("\nEngine type: " + EngineType);
+        }
+    }
+}
