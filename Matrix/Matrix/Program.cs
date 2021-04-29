eedeb72f082b5ed33db375cb4b91697d3fb9faa4
Alexey Myalik <myalick2017@yandex.ru>
2021-04-29 17:13:20 +0300

    Matrix task

---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int size = 5;
+                SquareMatrix squareMatrix = new SquareMatrix(size);
+                squareMatrix.RandomMatrixInitialization(0, 10);
+                Console.WriteLine(squareMatrix.ToString());
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
