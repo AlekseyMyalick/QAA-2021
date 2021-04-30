80eb80d72bd2b8fc4f2536a48320cbfbd9976b3e
Alexey Myalik <myalick2017@yandex.ru>
2021-04-30 18:30:09 +0300

    Creat and implement a class Point.

---
+++ HW-Triangle/HW-Triangle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 1);
+            Point p2 = new Point(10, 10);
+
+            Console.WriteLine(Point.Distance(p1, p2));
+        }
+    }
+}
