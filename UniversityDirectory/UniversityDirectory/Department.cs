37e87a2f9140471b2677bd4f6a3f88dc4d3206bf
Alexey Myalik <myalick2017@yandex.ru>
2021-05-19 22:19:17 +0300

    Add class department.

---
+++ UniversityDirectory/UniversityDirectory/Department.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace UniversityDirectory
+{
+    abstract class Department
+    {
+        public Person Leader { get; set; }
+
+        public List<Person> Staff { get; set; }
+
+        public Department (Person leader, List<Person> staff)
+        {
+            Leader = leader;
+            Staff = staff;
+        }
+
+        public Person this[int index]
+        {
+            get
+            {
+                return Staff[index];
+            }
+
+            set
+            {
+                Staff[index] = value;
+            }
+        }
+    }
+}
