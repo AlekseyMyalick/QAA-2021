c575f1f18a71ebc40bcc61c44795795aa098730c
Alexey Myalik <myalick2017@yandex.ru>
2021-05-19 22:24:06 +0300

    Add class Administration.

---
+++ UniversityDirectory/UniversityDirectory/Departments/Administration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityDirectory.Departments
+{
+    class Administration : Department
+    {
+        public Administration (Person leader, List<Person> staff) : base(leader, staff)
+        {
+
+        }
+    }
+}
