62e7c6fcd311c159e146cff254e24ecf597ce9f0
Alexey Myalik <myalick2017@yandex.ru>
2021-05-19 22:27:28 +0300

    Add class Person.

--- UniversityDirectory/UniversityDirectory/Person.cs
+++ UniversityDirectory/UniversityDirectory/Person.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UniversityDirectory
+﻿namespace UniversityDirectory
 {
-    class Person
+    abstract class Person
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public Person (string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
     }
 }
