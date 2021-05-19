a5849ef15713c5704428f80d1ee13856f0d80faf
Alexey Myalik <myalick2017@yandex.ru>
2021-05-19 22:31:53 +0300

    Add class Security.

---
+++ UniversityDirectory/UniversityDirectory/Departments/Security.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityDirectory.Departments
+{
+    class Security : Department
+    {
+        public Security(Person leader, List<Person> staff) : base(leader, staff)
+        {
+
+        }
+    }
+}
