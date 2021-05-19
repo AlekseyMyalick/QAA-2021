7e4625fccbc754af994ad560773117f5902c1e2f
Alexey Myalik <myalick2017@yandex.ru>
2021-05-19 22:28:53 +0300

    Add class Faculty.

---
+++ UniversityDirectory/UniversityDirectory/Departments/Faculty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityDirectory.Departments
+{
+    class Faculty : Department
+    {
+        public Faculty(Person leader, List<Person> staff) : base(leader, staff)
+        {
+
+        }
+    }
+}
