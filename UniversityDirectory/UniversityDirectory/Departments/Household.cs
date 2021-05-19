0ce44d3760d351f4a6144bf4fccfce0235ea3118
Alexey Myalik <myalick2017@yandex.ru>
2021-05-19 22:33:46 +0300

    Add class Household.

---
+++ UniversityDirectory/UniversityDirectory/Departments/Household.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityDirectory.Departments
+{
+    class Household : Department
+    {
+        public Household(Person leader, List<Person> staff) : base(leader, staff)
+        {
+
+        }
+    }
+}
